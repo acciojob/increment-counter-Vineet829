29db7fa09ebbe323655f356da1b07d716be0fe12
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2024-09-28 15:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-//your JS code here. If required.
+function increment(){
+	let temp = document.getElementById("counter")
+	temp.innerHTML = parseInt(temp.innerHTML) + parseInt(1)
+    alert(temp.innerHTML)
+}
